ee9e04106408d48e97ac1d08ba40d582e7a7c227
Elena <katayeva08@gmail.com>
2023-11-16 17:01:26 +0300

    change employee-add-form to class

--- src/components/employee-add-form/employee-add-form.js
+++ src/components/employee-add-form/employee-add-form.js
@@ -1,24 +1,48 @@
 
+import {Component} from 'react';
 import './employee-add-form.css';
 
-const EmployeeAddForm = () => {
-  return (
-    <div className="app-add-form">
-      <h3>Add a new employee</h3>
-      <form
-        className="add-form d-flex">
-        <input type="text"
-          className="form-control new-post-label"
-          placeholder="Employee's name?" />
-        <input type="number"
-          className="form-control new-post-label"
-          placeholder="Salary in $?" />
+class EmployeeAddForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      salary: ''
+    }
+  }
 
-        <button type="submit"
-          className="btn btn-outline-light">Add</button>
-      </form>
-    </div>
-  )
+  onValueChange = (e) => {
+    this.setState({
+      [e.target.name] : e.target.value
+    })
+  }
+  render () {
+    const {name,  salary} = this.state;
+    return (
+      <div className="app-add-form">
+        <h3>Add a new employee</h3>
+        <form
+          className="add-form d-flex">
+          <input type="text"
+            className="form-control new-post-label"
+            placeholder="Employee's name?"
+            name="name"
+            onChange={this.onValueChange}
+            value={name}/>
+          <input type="number"
+            className="form-control new-post-label"
+            placeholder="Salary in $?"
+            name="salary"
+            onChange={this.onValueChange}
+            value={salary}/>
+
+          <button type="submit"
+            className="btn btn-outline-light">Add</button>
+        </form>
+      </div>
+    )
+  }
+  
 }
 
 export default EmployeeAddForm;
